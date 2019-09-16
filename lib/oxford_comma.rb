4c0ca2ed4569ffa1386eb4339268e92e33c443d1
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-16 17:22:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   if array.length <= 2
     array.join(" and ")
-  else array.join[0..-2](",") + " and " + array[-1]
+  else array[0..-2].join(",") + " and " + array[-1]
   end
 end
